5f38ee13b3c50b37479f7a51d0965783862923f9
Anh Nguyen Cong <anh.nguyen13@ntq-solution.com.vn>
2021-06-04 08:26:37 +0700

    Initial Commit

---
+++ DemoEditVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoEditVideo
+//
+//  Created by Anhnguyen on 04/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
